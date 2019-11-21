0bc5a8a305fe8a06e565c328255b794a2fbdac50
Dusan Rychnovsky <durychno@microsoft.com>
2019-11-21 14:28:21 +0100

    Simplify admin-only access checks using a tag helper.

---
+++ Wishlist/TagHelpers/AuthorizationTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Wishlist.Views
+{
+    [HtmlTargetElement(Attributes = "asp-claim")]
+    public sealed class AuthorizationTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public AuthorizationTagHelper(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        [HtmlAttributeName("asp-claim")]
+        public string Claim { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!this.httpContextAccessor.HttpContext.User.HasClaim(this.Claim, "true"))
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
